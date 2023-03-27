9d993f3e88a103a07faf78046315ad9a8971dfce
Benjamin Hove <benhove86@gmail.com>
2023-03-27 13:35:33 -0400

    Advice generator

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const text = document.getElementById("text")
+const number = document.getElementById("number")
+const circle = document.getElementById("circle")
+
+function randomAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        number.innerHTML = `<small>${data.slip.id}</small>`
+        text.innerHTML = `<p>"${data.slip.advice}"</p>`
+    });
+}
+randomAdvice()
+
+circle.addEventListener("click", randomAdvice)
+
+
+
+
 
